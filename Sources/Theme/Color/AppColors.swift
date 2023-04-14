ba36a3d1b3450f24d9841e8bd9776180a964eea4
daniela.beltran <daniela.beltran@globant.com>
2023-04-13 19:43:23 -0500

    change public init

--- Sources/Theme/Color/AppColors.swift
+++ Sources/Theme/Color/AppColors.swift
@@ -7,23 +7,23 @@
 
 import SwiftUI
 public struct AppColors {
-    let  brand: Color
-    let  brandSecondary: Color
-    let  screenBackground: Color
-    let  modalBackground: Color
-    let  boxBackground: Color
-    let  textFieldBackground: Color
-    let  textPrimary: Color
-    let  textSecondary: Color
-    let  textButton: Color
-    let  buttonDisabled: Color
+    public let  brand: Color
+    public let  brandSecondary: Color
+    public let  screenBackground: Color
+    public let  modalBackground: Color
+    public let  boxBackground: Color
+    public let  textFieldBackground: Color
+    public  let  textPrimary: Color
+    public let  textSecondary: Color
+    public  let  textButton: Color
+    public let  buttonDisabled: Color
     
-    let  buttonDefault: Color
-    let  buttonPressed: Color
-    let  focusedColor: Color
-    let  unfocusedColor: Color
-    let  buttonLoader: Color
-    let buttonTextDisabled: Color
+    public let  buttonDefault: Color
+    public let  buttonPressed: Color
+    public let  focusedColor: Color
+    public let  unfocusedColor: Color
+    public let  buttonLoader: Color
+    public let buttonTextDisabled: Color
     
     public init(brand: Color, brandSecondary: Color, screenBackground: Color, modalBackground: Color, boxBackground: Color, textFieldBackground: Color, textPrimary: Color, textSecondary: Color, textButton: Color, buttonDisabled: Color, buttonDefault: Color, buttonPressed: Color, focusedColor: Color, unfocusedColor: Color, buttonLoader: Color, buttonTextDisabled: Color) {
         self.brand = brand
